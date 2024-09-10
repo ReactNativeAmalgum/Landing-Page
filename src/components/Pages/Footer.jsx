151084d0a5cec1a7ab5aa2d388d667be615d494c
ReactNativeAmalgum <137068385+ReactNativeAmalgum@users.noreply.github.com>
2024-09-10 16:10:30 +0530

    Mapp added

--- src/components/Pages/Footer.jsx
+++ src/components/Pages/Footer.jsx
@@ -19,17 +19,30 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <>
+    <section>
+    <iframe
+          src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3772.569649760874!2d73.11375047520325!3d18.994604082193266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTjCsDU5JzQwLjYiTiA3M8KwMDYnNTguOCJF!5e0!3m2!1sen!2sin!4v1725964585526!5m2!1sen!2sin"
+          width={'100%'}
+          height={450}
+          style={{ border: 0, marginBottom:0, bottom:0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+
+    </section>
       <section className="bottom-bar-cont">
+
         <div className="bar" />
         <div className="container">
           <div className="footer-row">
             <div className="footer-col-6">
               <h3 style={{ paddingTop: 5 }}>Abouts</h3>
               <p>
-              Lorem ipsum odor amet, consectetuer adipiscing elit. Senectus elementum penatibus praesent penatibus per nam rhoncus.
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Senectus elementum penatibus praesent penatibus per nam rhoncus.
               </p>
               <div className=" logo-cont">
-              <h2>Logo</h2>
+                <h2>Logo</h2>
               </div>
             </div>
             <div className="footer-nav-cont  col-2">
@@ -51,12 +64,12 @@ export default function Footer() {
               </p>
               <p>
                 <a className="footer-nav-cont-p" to="#properties">
-                Lands
+                  Lands
                 </a>
               </p>
               <p>
                 <a className="footer-nav-cont-p" to="#Agents">
-                Agents
+                  Agents
                 </a>
               </p>
               <p>
@@ -89,8 +102,8 @@ export default function Footer() {
               </div>
               <div className="social">
                 <FaMapPin className="icon" />{" "}
-                <p  > office no.-405, 4th floor, Neelkanth land mark, <br/> panvel 410206 
-</p>
+                <p  > office no.-405, 4th floor, Neelkanth land mark, <br /> panvel 410206
+                </p>
               </div>
             </div>
           </div>
